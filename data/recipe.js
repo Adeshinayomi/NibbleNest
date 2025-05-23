6afa35fbdd5971a5fe8fefaa225729d8fff8f3f5
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2025-05-23 18:04:21 +0100

    designed the homepage header and hero section

--- data/recipe.js
+++ data/recipe.js
@@ -3,14 +3,13 @@ async function fetchRecipes() {
   try {
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
     if (!response.ok) {
-      throw new Error("Failed to fetch questions");
+      throw new Error("Failed to fetch Recipes");
     }
     const data = await response.json();
     recipes=data.categories
-    console.log(recipes)
-
+    console.log(recipes);
   } catch (error) {
-    console.error("Error fetching quiz questions:", error);
+    console.error("Error fetching Recipes:", error);
     return [];
   }
 }
