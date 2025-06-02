b4f86c0b4db6f589a76fb50695fa4be4d13596c0
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2025-06-02 10:44:31 +0100

    fixed issues of overlapping in the about page

--- scripts/component/header.js
+++ scripts/component/header.js
@@ -2,13 +2,15 @@
   export function toggleSideBar(){
     const hamBurger=document.querySelector('.js-hamburger');
     const sideBar=document.querySelector('.js-sidebar')
-
+    const hero=document.querySelector('.js-hero')
     hamBurger.addEventListener('click',()=>{ 
-      if(hamBurger.innerHTML !== '<img src="/images/icons/button.png">'){
-        hamBurger.innerHTML='<img src="/images/icons/button.png">'
+      if(hamBurger.innerHTML !== '<img src="images/icons/button.png" alt="">'){
+        hamBurger.innerHTML='<img src="images/icons/button.png" alt="">'
       }else{
-        hamBurger.innerHTML='<img src="/images/icons/hamburger.png">'
+        hamBurger.innerHTML='<img src="images/icons/hamburger.png" alt="">'
       }
+      hero.classList.toggle('hide')
+      hamBurger.classList.toggle('grow')
       sideBar.classList.toggle('visible');
     })
   }
