c989e45627b433ab3e4acac85a0c80f6d7814fd6
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2025-06-28 19:19:34 +0100

    fixed design errors

--- scripts/about.js
+++ scripts/about.js
@@ -34,7 +34,4 @@ countDown('reviews',150,'.js-customer-review')
 //   },5)
 
 // }
-// countDown()
-
-const array1=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-console.log(array1.filter(num=>num>2))
+// countDown() 
